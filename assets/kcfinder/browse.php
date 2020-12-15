73817be9afd05a9e18f062a45b0ebff5bdba57a7
netindo <npos.cs@gmail.com>
2020-12-15 14:20:08 +0700

    merge

--- assets/kcfinder/browse.php
+++ assets/kcfinder/browse.php
@@ -1,24 +1,19 @@
 <?php
 
 /** This file is part of KCFinder project
-  *
-  *      @desc Browser calling script
-  *   @package KCFinder
-  *   @version 2.51
-  *    @author Pavel Tzonkov <pavelc@users.sourceforge.net>
-  * @copyright 2010, 2011 KCFinder Project
-  *   @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
-  *   @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
-  *      @link http://kcfinder.sunhater.com
-  */
+ *
+ *      @desc Browser calling script
+ *   @package KCFinder
+ *   @version 2.51
+ *    @author Pavel Tzonkov <pavelc@users.sourceforge.net>
+ * @copyright 2010, 2011 KCFinder Project
+ *   @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
+ *   @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
+ *      @link http://kcfinder.sunhater.com
+ */
 
 require "core/autoload.php";
 $browser = new browser();
 $browser->action();
 
-ob_start();
-require_once('../../index.php'); //path to main index file edited above
-ob_end_clean();
-$CI =& get_instance();
-$CI->load->library('session');
 ?>
